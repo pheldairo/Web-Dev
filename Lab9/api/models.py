2044617e3f567c2c8f55ec09c9b2b183cfa38573
Danya <lephiador@gmail.com>
2026-03-30 23:54:40 +0500

    Lab8/Lab9

---
+++ Lab9/api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default='')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+
+    def __str__(self):
+        return self.name
